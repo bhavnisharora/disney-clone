e358b19e780f15c3d49a28eb7e1f552f1506f226
bhavnisharora <bhavnisharora786@gmail.com>
2024-06-23 21:56:29 +0530

    starter

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+
+function App() {
+
+  return (
+    <>
+      <h1 className="bg-red-700 text-2xl font-sans">
+        disney clonessss
+      </h1>
+      <h1 className="text-3xl font-bold underline">
+        Hello world!
+      </h1>
+    </>
+  )
+}
+
+export default App
