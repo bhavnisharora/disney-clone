b8c224f03eeb1fc911aae4b62d26366c10eefe61
bhavnisharora <bhavnisharora786@gmail.com>
2024-06-24 16:04:59 +0530

    added navbar

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import logo from '../assets/logo.png'
 import { HiPlayCircle, HiTv, HiStar, HiMagnifyingGlass, HiHome } from "react-icons/hi2";
 import { HiPlus, HiDotsVertical } from "react-icons/hi";
@@ -31,14 +31,43 @@ const MenuItems = [
     }
 ];
 const Header = () => {
+
+    const [toggle, setToggle] = useState(false);
+
     return (
-        <div>
-            <img src={logo} className='w-[80px] md:w-[115px] object-cover' alt="" />
-            {MenuItems.map((item) => (
-                <div>
-                    <HeaderItem name={item.name} Icon={item.Icon} />
+        <div className='flex items-center justify-between p-5'>
+            <div className='flex gap-8 items-center'>
+                <img src={logo} className='w-[80px] md:w-[115px] object-cover' alt="" />
+                <div className='hidden md:flex gap-8'>
+                    {MenuItems.map((item) => (
+                        <div>
+                            <HeaderItem name={item.name} Icon={item.Icon} />
+                        </div>
+                    ))}
+
                 </div>
-            ))}
+
+                <div className='flex md:hidden gap-8'>
+                    {MenuItems.map((item, index) => index < 3 && (
+                        <div>
+                            <HeaderItem name={''} Icon={item.Icon} />
+                        </div>
+                    ))}
+                    <div className='md:hidden' onClick={() => setToggle(!toggle)}>
+                        <HeaderItem name={''} Icon={HiDotsVertical} />
+                        {toggle ? <div className='absolute w-[160px] mt-3 bg-[#121212] border-[1px] border-gray-700 p-3 px-5 py-4'>
+                            {MenuItems.map((item, index) => index > 2 && (
+                                <div>
+                                    <HeaderItem name={item.name} Icon={item.Icon} />
+                                </div>
+                            ))}
+                        </div> : null}
+                    </div>
+                </div>
+
+            </div>
+            <img src="https://ps.w.org/user-avatar-reloaded/assets/icon-128x128.png?rev=2540745" alt=""
+                className='w-[40px] rounded-full' />
         </div>
     )
 }
--- src/components/HeaderItem.jsx
+++ src/components/HeaderItem.jsx
@@ -1,11 +1,14 @@
 import React from 'react'
 
 const HeaderItem = ({ name, Icon }) => {
+
     return (
-        <div className='text-white flex items-center'>
+        <div className='text-white flex items-center gap-3 text-[15px] font-semibold cursor-pointer hover:underline
+        underline-offset-8 mb-3'>
             <Icon />
-            <h2>{name}</h2>
+            <h2 className=''> {name} </h2>
         </div>
+
     )
 }
 
