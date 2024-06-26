39e7abea716097fcce08c5c54d4d55124edafdb2
bhavnisharora <bhavnisharora786@gmail.com>
2024-06-26 11:07:51 +0530

    completed

--- src/components/GenreMovieList.jsx
+++ src/components/GenreMovieList.jsx
@@ -8,7 +8,7 @@ const GenreMovieList = () => {
             {GenersList.genere.map((item, index) => index <= 4 && (
                 <div key={item.id} className='p-8 px-8 md:px-16'>
                     <h2 className='text-[20px] text-white font-bold'>{item.name}</h2>
-                    <MovieList genreId={item.id} />
+                    <MovieList genreId={item.id} index_={index} />
                 </div>
             ))}
         </div>
--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,7 +1,14 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import GlobalApi from '../services/GlobalApi'
+import MovieCard from './MovieCard';
+import { MdKeyboardArrowLeft, MdKeyboardArrowRight } from "react-icons/md";
+import HrMovieCard from './HrMovieCard';
+const screenWidth = window.innerWidth;
 
-const MovieList = ({ genreId }) => {
+const MovieList = ({ genreId, index_ }) => {
+    const elemRef = useRef(null);
+
+    const [movieList, setMovieList] = useState([]);
 
     useEffect(() => {
         getMovieByGenereId();
@@ -10,13 +17,42 @@ const MovieList = ({ genreId }) => {
     const getMovieByGenereId = () => {
         GlobalApi.getMovieByGenereId(genreId).then((response) => {
             console.log(response.data.results);
+            setMovieList(response.data.results);
         })
 
     }
+    const slideRight = () => {
+        elemRef.current.scrollLeft += screenWidth - 110;
+    };
 
+    const slideLeft = () => {
+        elemRef.current.scrollLeft -= screenWidth - 110;
+    };
     return (
-        <div>
-            movielist
+        <div className='relative'>
+
+            <MdKeyboardArrowLeft
+                className={`hidden md:block text-white text-[40px] absolute left-[-40px] top-1/2 transform -translate-y-1/2 cursor-pointer `}
+                onClick={slideLeft}
+            />
+            <MdKeyboardArrowRight
+                className='hidden md:block text-white text-[40px] absolute right-[-40px] top-1/2 transform -translate-y-1/2 cursor-pointer'
+                onClick={slideRight}
+            />
+
+            <div ref={elemRef} className='flex overflow-x-auto gap-8 scroll-smooth scrollbar-none
+        pt-5 px-3 pb-5'>
+                {movieList.map((item, index) => (
+                    <>
+                        {
+                            index_ % 3 == 0 ? <HrMovieCard movie={item} /> : <MovieCard movie={item} />
+                        }
+                    </>
+                ))}
+            </div>
+
+
+
         </div>
     )
 }
